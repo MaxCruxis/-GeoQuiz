8ff700f6c437d5f4d673c11046b57a6df22c1a7e
maxcruxis <mailforprog@yandex.ru>
2021-07-10 17:07:14 +0300

    added "next" button and its activity to go next questions

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -5,12 +5,24 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+            Question(R.string.question_australia, true),
+            Question(R.string.question_oceans, true),
+            Question(R.string.question_mideast, false),
+            Question(R.string.question_africa, false),
+            Question(R.string.question_americas, true),
+            Question(R.string.question_asia, true))
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             val toast = Toast.makeText(
@@ -26,7 +40,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
 
             )
-            toast.setGravity(Gravity.TOP,0,0)
+            toast.setGravity(Gravity.TOP, 0, 0)
             toast.show()
         }
         falseButton.setOnClickListener { view: View ->
@@ -35,9 +49,20 @@ class MainActivity : AppCompatActivity() {
                     R.string.incorrect_toast,
                     Toast.LENGTH_SHORT
             )
-            toast.setGravity(Gravity.TOP,0,0)
+            toast.setGravity(Gravity.TOP, 0, 0)
             toast.show()
 
         }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
     }
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
 }
